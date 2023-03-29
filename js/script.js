f792ed338d7185acecf93f2333a9fa271bc1c719
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-03-29 19:40:24 -0300

    realizado tabuada simples com js

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ function tabuada() {
     } else {
         let n = Number(num.value)
         let c = 1
+        tab.innerHTML = ''//resetar a tabuada antes de mostrar uma nova.
         while (c <= 10) {
             let item = document.createElement('option');
             item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab ${c}`
             tab.appendChild(item)
             c++ //comando para incrementar o valor de c
         }
